2b9ca69d4144a52b9737dce6456a717396eae5b5
Saoven <andreas.orwii@gmail.com>
2021-11-10 15:48:15 +0100

    hide thermal gears because of unify (emendatus enigmatica)

--- kubejs/client_scripts/Ultimate Progression/JEI_hide.js
+++ kubejs/client_scripts/Ultimate Progression/JEI_hide.js
@@ -12,7 +12,15 @@ onEvent('jei.hide.items', event => {
         'mysticalagriculture:gaia_spirit_seeds',
         'mysticalagriculture:gaia_spirit_essence',
         'mysticalagriculture:tungsten_essence',
-        'mysticalagriculture:tungsten_seeds'
+        'mysticalagriculture:tungsten_seeds',
+        'thermal:enderium_gear',
+        'thermal:lumium_gear',
+        'thermal:signalum_gear',
+        'thermal:constantan_gear',
+        'thermal:invar_gear',
+        'thermal:lapis_gear',
+        'thermal:electrum_gear',
+        'thermal:bronze_gear'
     ];
         jeihide.forEach((entry) => {
         event.hide(entry);
